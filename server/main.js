e8854621cbed42171b8f17e5963da9bdde1eb8f6
cubomx <cheservelram657@gmail.com>
2021-04-01 16:44:14 -0600

    UPDATE: Already working server

--- server/main.js
+++ server/main.js
@@ -7,18 +7,22 @@ const port = process.env.PORT || 3001;
 
 const name = 'Sergio' || process.env.NAME;
 
-app.get("/api", (req, res) => {
-    res.json({message: 'Hello from server!'});
-    //res.send(`<h1>Hello, ${name}</h1>`)
-});
+
 
 
 app.listen(port, () => {
     console.log(`Listening at ${port} in localhost`);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+
+
+  app.get("/api", (req, res) => {
+    res.json({message: 'Hello from server!'});
+    //res.send(`<h1>Hello, ${name}</h1>`)
+});
 
   app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
